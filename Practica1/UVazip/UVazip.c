e5d2a642037204d22b38544643c65e06242c81cc
David <forexsencillo@gmail.com>
2024-02-22 00:29:09 +0100

    Test1
    Cristo bendito

--- Practica1/UVazip/UVazip.c
+++ Practica1/UVazip/UVazip.c
@@ -1,12 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+//char *imprimirBinario(int numero) {
+//    char cad[255];
+//    for (int i = sizeof(int) * 8 - 1; i >= 0; i--) {
+//        int bit = (numero >> i) & 1;
+//        sprintf(cad, "%d", bit);
+//    }
+//    return cad;
+//}
+
 int main(int argc, char *argv[]) {
 	FILE *fichero;
 	int i = 1, cont = 0;
 	char c;
 	char preC = '7';
+//    char datos[255];
 
+    if (argc == 1) {
+        printf("UVazip: file1 [file2 ...]");
+        exit(1);
+    }
 	while (i < argc) {
 		if ((fichero = fopen(argv[i], "r")) == NULL) {
 			printf("UVazip: no puedo abrir fichero\n");
@@ -17,7 +31,10 @@ int main(int argc, char *argv[]) {
 		do {
 			c = fgetc(fichero);
 			if (preC != '7' && c != preC) {
-				printf("%d%c", cont, preC);
+//                itoa(cont, datos,2);
+                fwrite(&cont, sizeof(int), 1, stdout);
+//                imprimirBinario(cont);
+				printf("%c", preC);
 				cont = 1;
 			} else {
 				cont++;
