aa413b4a58602d453ca1aa6bcfa35a2f9974e8b7
David <forexsencillo@gmail.com>
2024-02-23 00:37:25 +0100

    UVazip acabado de una vez

--- Practica1/UVazip/UVazip.c
+++ Practica1/UVazip/UVazip.c
@@ -3,12 +3,11 @@
 
 int main(int argc, char *argv[]) {
 	FILE *fichero;
-	int i = 1, cont = 0;
-	char c;
-	char preC = '7';
+	int i = 1, cont = 1;
+	char c, preC = '_';
 
     if (argc == 1) {
-        printf("UVazip: file1 [file2 ...]");
+        printf("UVazip: file1 [file2 ...]\n");
         exit(1);
     }
 	while (i < argc) {
@@ -17,19 +16,23 @@ int main(int argc, char *argv[]) {
 			exit(1);
 		}
 		i++;
-
-		do {
-			c = fgetc(fichero);
-			if (preC != '7' && c != preC && c != EOF) {
+		if (preC == '_') {
+			preC = fgetc(fichero);
+		}
+		while ((c = fgetc(fichero)) != EOF) {	
+			if (c != preC) {
 		                fwrite(&cont, sizeof(int), 1, stdout);
 				printf("%c", preC);
 				cont = 1;
 			} else if (preC == c) {
 				cont++;
 			}
-			preC = c;
-		} while (c != EOF);
+            preC = c;
+		}
 		fclose(fichero);
 	}
+    fwrite(&cont, sizeof(int), 1, stdout);
+    printf("%c", preC);
+
 	return 0;
 }
