fea228932e82df7a7fd326d26c46d122977a2773
David <forexsencillo@gmail.com>
2024-03-17 03:50:45 +0100

    fin (falta limpiar)

--- Practica2/UVash.c
+++ Practica2/UVash.c
@@ -20,16 +20,16 @@ void trim(char *s);
 
 int main(int argc, char *argv[]) {
     FILE *fich;
-    char *linea, *comandos, *token, *redir; //*prog;
+    char *linea, *comandos, *token, *redir; //*argsRedir; //*prog;
     char **args;
     char error_message[30] = "An error has occurred\n";
-    int bandera = 1, itArgumentos, contComandos = 0, batch; // j = 0;
-    size_t tam = 255;
+    int bandera = 1, itArgumentos, contComandos = 0, batch, prim = 1; // j = 0;
+    size_t cant = 255; // Cantidad de bytes que va a leer el getline y se va a guardar en linea
 
     pid_t pid;
 
 
-    linea = malloc(255 * sizeof(char));
+    linea = malloc(255 * sizeof(char)); // Reserva de la cantidad justa de memoria :)
     comandos = malloc(255 * sizeof(char)); // Espacio 16 comandos, 255 por cada uno. DE SOBRA :)
     token = malloc(64 * sizeof(char));
     args = malloc(8 * 128 * sizeof(char));
@@ -39,30 +39,60 @@ int main(int argc, char *argv[]) {
             fprintf(stderr, "%s", error_message);
             exit(1);
         }
-
         batch = 1;
     } else if (argc == 1) { //                                            ---MODO INTERACTIVO---
         fich = stdin;
         batch = 0;
     } else { // ERROR CATASTRÓFICO
         fprintf(stderr, "%s", error_message);
+        exit(-1);
     }
 
     while (bandera) {
         if (!batch) printf("\033[1m\033[38;2;255;87;51mUVash> \033[0m");
 //        if (!batch) printf("UVash> "); // Para pasar los tests
-        getline(&linea, &tam, fich);
-        if (batch && linea == NULL) exit(0); /////////////////////////////////////////////
+//        pid = getline(&linea, &cant, fich);
+        if (getline(&linea, &cant, fich) == -1) {
+            if (prim) exit(1);
+            exit(0);
+        }
+        prim = 0;
+//        if(linea[0] ) exit(1);
+        trim(linea);
+        if (linea[0] == '&' || linea[0] == '>') {
+            fprintf(stderr, "%s", error_message);
+            continue; // TODO quitar esto
+        }
 
 //        printf("%s", linea);
 
         linea[strlen(linea) - 1] = '\0';
 
         contComandos = 0;
+        comsWhile:
         while ((comandos = strsep(&linea, "&")) != NULL) { //Separador de __COMANDOS__
 
             token = strsep(&comandos, ">");
+
+            trim(comandos);
+            if (comandos != NULL){
+                for (int i = 0; i < strlen(comandos); ++i) if (comandos[i] == ' ' || comandos[i] == '>') {
+                        fprintf(stderr, "%s", error_message);
+                        goto comsWhile;
+                    }
+            }
+//            printf("com:_%s_,redir:_%s_\n", comandos, redir);
             redir = strsep(&comandos, ">");
+            trim(redir);
+
+
+//            argsRedir = strsep(&redir, " ");
+//            if (comandos != NULL || argsRedir)
+//            printf("redir:_%s_\n", comandos);
+            if (redir != NULL && strcmp(redir, "") == 0) {
+                fprintf(stderr, "%s", error_message);
+                goto comsWhile;
+            }
 
             itArgumentos = 0; // Iterador de argumentos
             while ((args[itArgumentos] = strsep(&token, " ")) != NULL) { //Separador de __ARGUMENTOS__
@@ -73,12 +103,13 @@ int main(int argc, char *argv[]) {
                     itArgumentos++; // Avanza porfa
                 }
             }
-            //printf("Se ejecutará con: %s y %s en la salida %s\n", args[0], args[1], redir);
+//            printf("Se ejecutará con: %s y %s en la salida %s\n", args[0], args[1], redir);
 
             if (args[0] != NULL) {
                 if (strcmp("exit", args[0]) == 0) { // ------------EXIT??😎
+                    if (args[1] != NULL) fprintf(stderr, "%s", error_message);
+                    else exit(0);
 //                printf("Acaba el programa %d\n", getpid());
-                    exit(0);
                 } else if (strcmp("cd", args[0]) == 0) { // ------------CD??😎
                     if (args[1] == NULL || args[2] != NULL) { // argc será al menos 1 siempre (== 0)
                         fprintf(stderr, "%s", error_message);
@@ -87,12 +118,15 @@ int main(int argc, char *argv[]) {
                     if (getpid() != 0) { // SOY PADRE🧐
                         pid = fork(); // PE I DE
                         contComandos++;
+
                     }
 
                     if (pid == 0) {
                         bandera = ejecutar(args, redir);
                     }
                 }
+//            } else {
+//                fprintf(stderr, "%s", error_message);
             }
         }
 
@@ -120,7 +154,7 @@ int ejecutar(char *args[], char *redir) {
 
     // ------------NORMAL??😎
     if (redir != NULL) { // REDIRECCION 🤑
-        trim(redir);
+//        trim(redir);
 
         if ((fOut = open(redir, O_WRONLY | O_CREAT | O_TRUNC, 0666)) == -1) {
             fprintf(stderr, "An error has occurred\n");
